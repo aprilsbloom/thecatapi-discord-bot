fc3f77dc642b9faf51c6b18a5469ba8188085f79
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2022-06-16 17:37:14 +1200

    updated embeds

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const Discord = require('discord.js');
 const axios = require('axios');
 const { MessageEmbed } = require('discord.js');
 
+
+
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+
+const Errorhandler = require("discord-error-handler");
+const handle = new Errorhandler(client, {
+    webhook: { id: `webhook-id`, token: `webhook-token` } 
+})
+
+process.on('unhandledRejection', error => {
+    handle.createrr(client, undefined, undefined, error) // sends errors to a discord webhook and handles the errors, instead of putting the bot offline when it encounters an error
+});
 
 client.on('ready', () => {
     client.user.setActivity(`*help in ${client.guilds.cache.size} Servers `, { type: "LISTENING" })
@@ -12,6 +24,8 @@ client.on('ready', () => {
     console.log('Logs:');
 });
 
+
+
 const apikeys = ["put-api-key-here", "put-api-key-here"];
 
 const random = Math.floor(Math.random() * apikeys.length);
@@ -19,37 +33,37 @@ const random = Math.floor(Math.random() * apikeys.length);
 client.on('messageCreate', async msg => {
     switch (msg.content) {
         case "*gif":
-            const img = await getGif(); //fetches a URL from the API
-            const exampleEmbed = new MessageEmbed()
+            const gif = await getGif(); //fetches a URL from the API
+            const gifembed = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Here\'s your cat gif:')
-                .setURL(img)
-                .setImage(img)
-                .setFooter({ text: 'Made by business man#1542', iconURL: 'https://i.imgur.com/1AyUxRE.png' });
-            msg.channel.send({ embeds: [exampleEmbed] });
+                .setURL(gif)
+                .setImage(gif)
+                .setFooter({ text: 'Made by @gifkitties', iconURL: 'https://cdn.discordapp.com/attachments/889397754458169385/985133240098627644/ezgif-3-df748915d9.gif' });
+            msg.channel.send({ embeds: [gifembed] });
             console.log('User: ' + msg.author.tag + ' ID: ' + msg.author.id + ' ran *gif in Channel: #' + msg.channel.name + ' Server: ' + msg.guild.name)
             break;
         case "*image":
-            const img1 = await getImage();
-            const exampleEmbed1 = new MessageEmbed()
+            const img = await getImage();
+            const imgembed = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Here\'s your cat image:')
-                .setURL(img1)
-                .setImage(img1)
-                .setFooter({ text: 'Made by business man#1542', iconURL: 'https://i.imgur.com/1AyUxRE.png' });
-            msg.channel.send({ embeds: [exampleEmbed1] });
+                .setURL(img)
+                .setImage(img)
+                .setFooter({ text: 'Made by @gifkitties', iconURL: 'https://cdn.discordapp.com/attachments/889397754458169385/985133240098627644/ezgif-3-df748915d9.gif' });
+            msg.channel.send({ embeds: [imgembed] });
             console.log('User: ' + msg.author.tag + ' ID: ' + msg.author.id + ' ran *image in Channel: #' + msg.channel.name + ' Server: ' + msg.guild.name)
             break;
         case "*help":
-            const img2 = await getImage();
+            const help = await getImage();
             const exampleEmbed2 = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Help')
-                .setURL(img2)
-                .addFields({ name: 'Gif', value: '```*gif```Returns a random cat gif.', inline: true }, { name: 'Image', value: '```*image``` to get a random cat image.', inline: true }, )
+                .setURL(help)
+                .addFields({ name: 'Gif ', value: '```*gif``` Returns a random cat gif.', inline: true }, { name: 'Image ', value: '```*image``` Returns a random cat image.', inline: true }, )
                 .addField('Help', '```*help```Sends this message.', true)
-                .setImage(img2)
-                .setFooter({ text: 'Made by business man#1542', iconURL: 'https://i.imgur.com/1AyUxRE.png' });
+                .setImage(help)
+                .setFooter({ text: 'Made by @gifkitties', iconURL: 'https://cdn.discordapp.com/attachments/889397754458169385/985133240098627644/ezgif-3-df748915d9.gif' });
             msg.channel.send({ embeds: [exampleEmbed2] });
             console.log('User: ' + msg.author.tag + ' ID: ' + msg.author.id + ' ran *help in Channel: #' + msg.channel.name + ' Server: ' + msg.guild.name)
             break;
@@ -74,7 +88,6 @@ async function getImage() {
     });
     return res.data[0].url;
 }
-
 
 //make sure this is the last line
 client.login(process.env.CLIENT_TOKEN); //login to the bot using the token
